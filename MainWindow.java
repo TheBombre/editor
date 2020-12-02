0807f93b9e680fd75e612b4a5f6254ce3a4d8c2f
TheBombre <carltonndhlovu@Carltons-MBP.Home>
2020-12-02 18:49:41 +0200

    Add menubar with file and edit menu items, no functionality

--- MainWindow.java
+++ MainWindow.java
@@ -5,6 +5,23 @@ import java.awt.event.ActionListener;
 
 public class MainWindow extends JFrame {
 
+    //Bar
+    JMenuBar menuBar;
+
+    // Main menus
+    JMenu fileMenu;
+    JMenu editMenu;
+
+    // File Menu options
+    JMenuItem newFileOption;
+    JMenuItem openFileOption;
+    JMenuItem saveFileOption;
+
+    // Edit Menu options
+    JMenuItem pasteOption;
+    JMenuItem redoOption;
+    JMenuItem undoOption;
+
     public static void main(String[] args) {
         MainWindow window = new MainWindow();
     }
@@ -12,23 +29,40 @@ public class MainWindow extends JFrame {
     private final int HEIGHT = 500;
     private final int WIDTH = 700;
     public MainWindow() {
-        super("My super fun title!! yAyy");
+        super("Basic Text Editor");
         setSize(WIDTH, HEIGHT);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JMenuBar bar = new JMenuBar();
-	JMenu menu = new JMenu("Menu");
-	JMenu submenu = new JMenu("Submenu");
-        JMenuItem file = new JMenuItem("File");
-        JMenuItem create = new JMenuItem("New File");
+        menuBar = new JMenuBar();
 
-        file.addActionListener(new MenuItemListener());
-        menu.add(create);
-        submenu.add(file);
-        menu.add(submenu);
-        bar.add(menu);
+        // Initialise main menus
+        editMenu = new JMenu("Edit");
+	fileMenu = new JMenu("File");
 
-        setJMenuBar(bar);
+	// Initialise file menu options
+	newFileOption = new JMenuItem("New file");
+	openFileOption = new JMenuItem("Open file");
+	saveFileOption = new JMenuItem("Save file");
+
+	// Initialise edit menu options
+	pasteOption = new JMenuItem("Paste");
+	redoOption = new JMenuItem("Redo");
+	undoOption = new JMenuItem("Undo");
+
+	// Append file menu options to file menu
+	fileMenu.add(newFileOption);
+	fileMenu.add(openFileOption);
+	fileMenu.add(saveFileOption);
+
+	// Append edit menu options to edit menu
+	editMenu.add(pasteOption);
+	editMenu.add(redoOption);
+	editMenu.add(undoOption);
+
+	menuBar.add(fileMenu);
+	menuBar.add(editMenu);
+
+        setJMenuBar(menuBar);
         setVisible(true);
     }
 
